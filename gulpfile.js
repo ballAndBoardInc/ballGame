a624b486954ceaef7db869c299a0643ca4bc1a3d
Aaron Janke <janke.aaron@gmail.com>
2018-03-23 00:18:18 -0400

    done did da basics

--- gulpfile.js
+++ gulpfile.js
@@ -7,21 +7,21 @@ const browserSync = require('browser-sync').create();
 const reload = browserSync.reload;
 
 gulp.task('styles', () => {
-   return gulp.src('./dev/styles/style.scss')
+   return gulp.src('./client/src/styles/style.scss')
       .pipe(sass().on('error', sass.logError))
       .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1'))
       .pipe(concat('style.css'))
-      .pipe(gulp.dest('./public/styles'))
+      .pipe(gulp.dest('./client/public/styles'))
       .pipe(reload({ stream: true }));
 });
 
 gulp.task('scripts', () => {
-   return gulp.src('./dev/scripts/script.js')
+   return gulp.src('./client/src/scripts/script.js')
       .pipe(babel({
          presets: ['env']
       }))
       .pipe(concat('main.js'))
-      .pipe(gulp.dest('./public/scripts'))
+      .pipe(gulp.dest('./client/public/scripts'))
       .pipe(reload({ stream: true }));
 });
 
@@ -32,7 +32,7 @@ gulp.task('bs', () => {
 });
 
 gulp.task('default', ['styles', 'scripts', 'bs'], () => {
-   gulp.watch('./dev/**/*.scss', ['styles']);
-   gulp.watch('./dev/**/*.js', ['scripts']);
+   gulp.watch('./client/**/*.scss', ['styles']);
+   gulp.watch('./client/**/*.js', ['scripts']);
    gulp.watch('*.html', reload);
 });
