2a59309624f73d800bd86d4d3eb559487cef48f7
Aaron Janke <janke.aaron@gmail.com>
2018-03-26 21:48:54 -0400

    basic floating 3D image, updated gulpfile

--- client/gulpfile.js
+++ client/gulpfile.js
@@ -1,27 +1,40 @@
-const gulp = require('gulp');
-const sass = require('gulp-sass');
-const concat = require('gulp-concat');
-const babel = require('gulp-babel');
-const autoprefixer = require('gulp-autoprefixer');
-const browserSync = require('browser-sync').create();
-const reload = browserSync.reload;
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var concat = require('gulp-concat');
+var babel = require('gulp-babel');
+var browserify = require('browserify');
+var autoprefixer = require('gulp-autoprefixer');
+var browserSync = require('browser-sync').create();
+var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
+var notify = require('gulp-notify');
+var plumber = require('gulp-plumber');
+var historyApiFallback = require("connect-history-api-fallback");
+var reload = browserSync.reload;
 
 gulp.task('styles', () => {
-   return gulp.src('./client/src/styles/style.scss')
+   return gulp.src('./src/styles/style.scss')
       .pipe(sass().on('error', sass.logError))
       .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1'))
       .pipe(concat('style.css'))
-      .pipe(gulp.dest('./client/public/styles'))
+      .pipe(gulp.dest('./public/styles'))
       .pipe(reload({ stream: true }));
 });
 
 gulp.task('scripts', () => {
-   return gulp.src('./client/src/scripts/script.js')
-      .pipe(babel({
+   return browserify('./src/scripts/script.js', { debug: true })
+      .transform('babelify', {
+         sourceMaps: true,
          presets: ['env']
+      })
+      .bundle()
+      .on('error', notify.onError({
+         message: "Error: <%= error.message %>",
+         title: 'Error in JS 💀'
       }))
-      .pipe(concat('main.js'))
-      .pipe(gulp.dest('./client/public/scripts'))
+      .pipe(source('main.js'))
+      .pipe(buffer())
+      .pipe(gulp.dest('./public/scripts'))
       .pipe(reload({ stream: true }));
 });
 
@@ -32,7 +45,7 @@ gulp.task('bs', () => {
 });
 
 gulp.task('default', ['styles', 'scripts', 'bs'], () => {
-   gulp.watch('./client/**/*.scss', ['styles']);
-   gulp.watch('./client/**/*.js', ['scripts']);
+   gulp.watch('./**/*.scss', ['styles']);
+   gulp.watch('./**/*.js', ['scripts']);
    gulp.watch('*.html', reload);
 });
