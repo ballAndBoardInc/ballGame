18b8929b35406e56d655fdedcb8d303336f83e37
Aaron Janke <janke.aaron@gmail.com>
2018-03-31 15:52:47 -0400

    tilted playing field implemented

--- client/src/scripts/script.js
+++ client/src/scripts/script.js
@@ -22,13 +22,13 @@ window.addEventListener('DOMContentLoaded', function () {
 
       //CAMERA
       // This creates and positions a free camera (non-mesh)
-      var camera = new BABYLON.FreeCamera("camera1", new BABYLON.Vector3(0, 60, 0), scene);
+      var camera = new BABYLON.UniversalCamera("camera1", new BABYLON.Vector3(10, 0, 55), scene);
 
       // This targets the camera to scene origin
       camera.setTarget(BABYLON.Vector3.Zero());
 
       // This attaches the camera to the canvas
-      camera.attachControl(canvas, false);
+      // camera.attachControl(canvas, false);
 
       //LIGHTS
       // This creates a light, aiming 0,1,0 - to the sky (non-mesh)
@@ -39,33 +39,32 @@ window.addEventListener('DOMContentLoaded', function () {
 
       //OBJECTS
       // Our built-in 'sphere' shape. Params: name, subdivs, size, scene
-      var sphere = BABYLON.Mesh.CreateSphere("sphere1", 16, 2, scene);
-
-      // //OBJECT OPTIONS
-      var boxOptions = {
-         height: 1,
-         width: 1,
-         depth: 0.5
-      };
+      // var sphere = BABYLON.Mesh.CreateSphere("sphere1", 16, 2, scene);
 
       //BOXES
-      var testBox = BABYLON.MeshBuilder.CreateBox("testBox", boxOptions, scene);
+      // var testBox = BABYLON.MeshBuilder.CreateBox("testBox", { height: getRandom(0.5, 3), width: getRandom(0.5, 3), depth: getRandom(0.5, 3) }, scene);
 
       // Our built-in 'ground' shape. Params: name, width, depth, subdivs, scene
-      var ground = BABYLON.Mesh.CreateGround("ground1", 15, 15, 2, scene);
+      var ground = BABYLON.Mesh.CreateGround("ground1", 15, 45, 2, scene);
 
-      // Move the sphere upward 1/2 its height
-      sphere.position.y = 5;
-      testBox.position.y = 2;
-      testBox.position.x = 1;
+      //PLANE
+      // var myPlane = BABYLON.MeshBuilder.CreatePlane("myPlane", { width: 5, height: 2 }, scene);
+
+      //POSITION AND ROTATE
+      ground.position.z = 10;
+      ground.rotate(BABYLON.Axis.X, Math.PI / 9, BABYLON.Space.WORLD);
+      // mesh.rotate(BABYLON.Axis.X, Math.PI / 4, BABYLON.Space.WORLD);
+      // sphere.position.y = 5;
+      // testBox.position.y = 2;
+      // testBox.position.x = 1;
 
       //PHYSICS
       scene.enablePhysics();
 
       //PHYSICS IMPOSTERS
-      sphere.physicsImpostor = new BABYLON.PhysicsImpostor(sphere, BABYLON.PhysicsImpostor.SphereImpostor, { mass: 1, restitution: 0.9 }, scene);
+      // sphere.physicsImpostor = new BABYLON.PhysicsImpostor(sphere, BABYLON.PhysicsImpostor.SphereImpostor, { mass: 1, restitution: 0.9 }, scene);
 
-      testBox.physicsImpostor = new BABYLON.PhysicsImpostor(testBox, BABYLON.PhysicsImpostor.SphereImpostor, { mass: 1, restitution: 0.9 }, scene);
+      // testBox.physicsImpostor = new BABYLON.PhysicsImpostor(testBox, BABYLON.PhysicsImpostor.SphereImpostor, { mass: 1, restitution: 0.9 }, scene);
 
       ground.physicsImpostor = new BABYLON.PhysicsImpostor(ground, BABYLON.PhysicsImpostor.BoxImpostor, { mass: 0, restitution: 0.9 }, scene);
 
@@ -74,20 +73,28 @@ window.addEventListener('DOMContentLoaded', function () {
       let i = 1;
       let acceleration = 1000;
       
-
       function interval(acceleration) {
          const interval = setInterval(newShape, acceleration);
       }
 
       function newShape() {
-         i = i + 1;
+         if (i % 2 === 0) {
+            var newSphere = BABYLON.Mesh.CreateSphere("genSphere", 16, getRandom(0.5, 3), scene);
+            newSphere.position.y = getRandom(10, 15);
+            newSphere.position.x = getRandom(-5, 5);
+            newSphere.position.z = getRandom(-5, 5);
+            newSphere.physicsImpostor = new BABYLON.PhysicsImpostor(newSphere, BABYLON.PhysicsImpostor.SphereImpostor, { mass: getRandom(10, 20), restitution: getRandom(0.7, 1.2) }, scene);
+         }
+        if (i % 2 === 1) {
+            var newBox = BABYLON.MeshBuilder.CreateBox("genBox", { height: getRandom(0.5, 3), width: getRandom(0.5, 3), depth: getRandom(0.5, 3) }, scene);
+            newBox.position.y = getRandom(10, 15);
+            newBox.position.x = getRandom(-5, 5);
+            newBox.position.z = getRandom(-5, 5);
+            newBox.physicsImpostor = new BABYLON.PhysicsImpostor(newBox, BABYLON.PhysicsImpostor.BoxImpostor, { mass: getRandom(10, 20), restitution: getRandom(0.7, 1.2) }, scene);
+         }
+         i++;
          acceleration = acceleration + 100;
          console.log(i, acceleration);
-         var newSphere = BABYLON.Mesh.CreateSphere("sphere1", 16, getRandom(0.5, 2), scene);
-         newSphere.position.y = getRandom(10, 15);
-         newSphere.position.x = getRandom(-5, 5);
-         newSphere.position.z = getRandom(-5, 5);
-         newSphere.physicsImpostor = new BABYLON.PhysicsImpostor(newSphere, BABYLON.PhysicsImpostor.SphereImpostor, { mass: 1, restitution: 0.9 }, scene);
       };
       interval(acceleration);
 
