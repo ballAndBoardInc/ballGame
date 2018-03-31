ace45d42c14bbf3dbea6676743d8ef725acc2386
Aaron Janke <janke.aaron@gmail.com>
2018-03-31 16:48:18 -0400

    adding colors and textures

--- client/src/scripts/script.js
+++ client/src/scripts/script.js
@@ -20,6 +20,9 @@ window.addEventListener('DOMContentLoaded', function () {
       // This creates a basic Babylon Scene object (non-mesh)
       var scene = new BABYLON.Scene(engine);
 
+      //PHYSICS
+      scene.enablePhysics();
+
       //CAMERA
       // This creates and positions a free camera (non-mesh)
       var camera = new BABYLON.UniversalCamera("camera1", new BABYLON.Vector3(10, 0, 55), scene);
@@ -28,7 +31,7 @@ window.addEventListener('DOMContentLoaded', function () {
       camera.setTarget(BABYLON.Vector3.Zero());
 
       // This attaches the camera to the canvas
-      // camera.attachControl(canvas, false);
+      camera.attachControl(canvas, false);
 
       //LIGHTS
       // This creates a light, aiming 0,1,0 - to the sky (non-mesh)
@@ -38,35 +41,46 @@ window.addEventListener('DOMContentLoaded', function () {
       light.intensity = 0.7;
 
       //OBJECTS
-      // Our built-in 'sphere' shape. Params: name, subdivs, size, scene
-      // var sphere = BABYLON.Mesh.CreateSphere("sphere1", 16, 2, scene);
+      // PLAYER SPHERE Our built-in 'sphere' shape. Params: name, subdivs, size, scene
+      var playerMesh = BABYLON.Mesh.CreateSphere("playerSphere", 16, 1.5, scene);
+      var playerMaterial = new BABYLON.StandardMaterial("playerMaterial", scene);
+      playerMaterial.diffuseColor = new BABYLON.Color3(255, 255, 255);
+      // playerMaterial.ambientColor = new BABYLON.Color3(0.23, 0.98, 0.53);
+      playerMaterial.wireframe = true;
+      playerMesh.material = playerMaterial;
+
+      // PLAYER IMPOSTER
+      playerMesh.physicsImpostor = new BABYLON.PhysicsImpostor(playerMesh, BABYLON.PhysicsImpostor.SphereImpostor, { mass: 5, restitution: 0.9 }, scene);
 
       //BOXES
       // var testBox = BABYLON.MeshBuilder.CreateBox("testBox", { height: getRandom(0.5, 3), width: getRandom(0.5, 3), depth: getRandom(0.5, 3) }, scene);
 
-      // Our built-in 'ground' shape. Params: name, width, depth, subdivs, scene
-      var ground = BABYLON.Mesh.CreateGround("ground1", 15, 45, 2, scene);
+      //GROUND Our built-in 'ground' shape. Params: name, width, depth, subdivs, scene
+      // var ground = BABYLON.Mesh.CreateGround("ground1", 15, 45, 2, scene);
+      // var groundMaterial = new BABYLON.StandardMaterial("playerMaterial", scene);
+      // groundMaterial.diffuseColor = new BABYLON.Color3(0, 0, 0);
+      // ground.material = groundMaterial;
+
+      //GROUND IMPOSTER
+      // ground.physicsImpostor = new BABYLON.PhysicsImpostor(ground, BABYLON.PhysicsImpostor.BoxImpostor, { mass: 0, restitution: 0.9 }, scene);
 
       //PLANE
-      // var myPlane = BABYLON.MeshBuilder.CreatePlane("myPlane", { width: 5, height: 2 }, scene);
+      var gamePlane = BABYLON.MeshBuilder.CreatePlane("gamePlane", { width: 15, height: 45, sideOrientation: BABYLON.Mesh.DOUBLESIDE }, scene);
+      var gamePlaneMaterial = new BABYLON.StandardMaterial("playerMaterial", scene);
+      gamePlaneMaterial.diffuseColor = new BABYLON.Color3(0, 0, 0);
+      gamePlane.material = gamePlaneMaterial;
+
+      //GAME PLANE IMPOSTER
+      gamePlane.physicsImpostor = new BABYLON.PhysicsImpostor(gamePlane, BABYLON.PhysicsImpostor.BoxImpostor, { mass: 0, restitution: 0.9 }, scene);
 
       //POSITION AND ROTATE
-      ground.position.z = 10;
-      ground.rotate(BABYLON.Axis.X, Math.PI / 9, BABYLON.Space.WORLD);
+      playerMesh.position.z = 15;
+      gamePlane.position.z = 10;
+      gamePlane.rotate(BABYLON.Axis.X, Math.PI / 1.7, BABYLON.Space.WORLD);
       // mesh.rotate(BABYLON.Axis.X, Math.PI / 4, BABYLON.Space.WORLD);
       // sphere.position.y = 5;
       // testBox.position.y = 2;
       // testBox.position.x = 1;
-
-      //PHYSICS
-      scene.enablePhysics();
-
-      //PHYSICS IMPOSTERS
-      // sphere.physicsImpostor = new BABYLON.PhysicsImpostor(sphere, BABYLON.PhysicsImpostor.SphereImpostor, { mass: 1, restitution: 0.9 }, scene);
-
-      // testBox.physicsImpostor = new BABYLON.PhysicsImpostor(testBox, BABYLON.PhysicsImpostor.SphereImpostor, { mass: 1, restitution: 0.9 }, scene);
-
-      ground.physicsImpostor = new BABYLON.PhysicsImpostor(ground, BABYLON.PhysicsImpostor.BoxImpostor, { mass: 0, restitution: 0.9 }, scene);
 
       //INTERVAL AND SHAPE GENERATOR
 
@@ -80,17 +94,29 @@ window.addEventListener('DOMContentLoaded', function () {
       function newShape() {
          if (i % 2 === 0) {
             var newSphere = BABYLON.Mesh.CreateSphere("genSphere", 16, getRandom(0.5, 3), scene);
+            // var newSphereMesh = BABYLON.Mesh.CreateSphere("playerSphere", 16, 2, scene);
+            
+            var newSphereMaterial = new BABYLON.StandardMaterial("playerMaterial", scene);
+            newSphereMaterial.diffuseColor = new BABYLON.Color3(getRandom(0.5, 6), getRandom(0.5, 6), getRandom(0.5, 3));
+            newSphere.material = newSphereMaterial;
             newSphere.position.y = getRandom(10, 15);
             newSphere.position.x = getRandom(-5, 5);
             newSphere.position.z = getRandom(-5, 5);
-            newSphere.physicsImpostor = new BABYLON.PhysicsImpostor(newSphere, BABYLON.PhysicsImpostor.SphereImpostor, { mass: getRandom(10, 20), restitution: getRandom(0.7, 1.2) }, scene);
+            newSphere.physicsImpostor = new BABYLON.PhysicsImpostor(newSphere, BABYLON.PhysicsImpostor.SphereImpostor, { mass: getRandom(10, 20), restitution: getRandom(0.8, 1.2) }, scene);
          }
         if (i % 2 === 1) {
             var newBox = BABYLON.MeshBuilder.CreateBox("genBox", { height: getRandom(0.5, 3), width: getRandom(0.5, 3), depth: getRandom(0.5, 3) }, scene);
+
+            var newBoxMaterial = new BABYLON.StandardMaterial("playerMaterial", scene);
+            newBoxMaterial.diffuseColor = new BABYLON.Color3(getRandom(0.5, 6), getRandom(0.5, 6), getRandom(0.5, 3));
+            newBox.material = newBoxMaterial;
+            
+            newBox.rotate(BABYLON.Axis.X, Math.PI / getRandom(1, 4), BABYLON.Space.WORLD);
+            newBox.rotate(BABYLON.Axis.Y, Math.PI / getRandom(1, 4), BABYLON.Space.WORLD);
             newBox.position.y = getRandom(10, 15);
             newBox.position.x = getRandom(-5, 5);
             newBox.position.z = getRandom(-5, 5);
-            newBox.physicsImpostor = new BABYLON.PhysicsImpostor(newBox, BABYLON.PhysicsImpostor.BoxImpostor, { mass: getRandom(10, 20), restitution: getRandom(0.7, 1.2) }, scene);
+            newBox.physicsImpostor = new BABYLON.PhysicsImpostor(newBox, BABYLON.PhysicsImpostor.BoxImpostor, { mass: getRandom(10, 20), restitution: getRandom(0.8, 1.2) }, scene);
          }
          i++;
          acceleration = acceleration + 100;
